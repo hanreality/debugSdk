90e57ed0911cd4497e21306a9f3794dab358c8c6
han.chen <han.chen@beibei.com>
2022-01-18 13:25:39 +0800

    Keep

--- debug_sdk/src/main/java/com/melot/android/debug/sdk/kit/toolpanel/KitBeans.kt
+++ debug_sdk/src/main/java/com/melot/android/debug/sdk/kit/toolpanel/KitBeans.kt
@@ -1,4 +1,8 @@
 package com.melot.android.debug.sdk.kit.toolpanel
 
+import androidx.annotation.Keep
+
+@Keep
 data class KitGroupBean(var groupId: String, var kits: MutableList<KitBean>)
+@Keep
 data class KitBean(var allClassName: String, var checked: Boolean, var innerKitId: String = "")
