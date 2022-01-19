91f2cebe9f87f1241e1567da5444459463d4b393
han.chen <han.chen@beibei.com>
2022-01-19 16:07:46 +0800

    混淆问题

--- debug_sdk/src/main/java/com/melot/android/debug/sdk/model/DebugTestAccountModel.kt
+++ debug_sdk/src/main/java/com/melot/android/debug/sdk/model/DebugTestAccountModel.kt
@@ -1,9 +1,12 @@
 package com.melot.android.debug.sdk.model
 
+import androidx.annotation.Keep
+
 /**
  * Author: han.chen
  * Time: 2021/11/23 16:30
  */
+@Keep
 data class DebugTestAccountModel(
     var success: Boolean,
     var data: ArrayList<DebugTestAccountData>?
